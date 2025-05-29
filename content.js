6d8d715b96f9349095425e9279efbbf700e25e9b
Landon Duba <landon.duba@gmail.com>
2025-05-29 00:03:26 -0400

    Added banner upload (which needed name upload)

--- content.js
+++ content.js
@@ -7,18 +7,47 @@ const YOUR_USER_ID = "1234248313432182844"; // Replace with your ID from the URL
 let YOUR_GIF_URL = "";
 
 
-async function getInfo() {
+async function getGifUrl() {
   const result = await chrome.storage.local.get(["gif"]);
-  const id = await chrome.storage.local.get(["userId"]);
-  console.log("THE ID IS", id.userId);
   console.log("THE GIF URL IS", result.gif);
   return result.gif;
 }
 
+async function getId() {
+  const id = await chrome.storage.local.get(["userId"]);
+  console.log("THE ID IS", id.userId);
+  return id.userId;
+}
+
+
+async function getName() {
+  const name = await chrome.storage.local.get(["name"]);
+  console.log("THE NAME IS", name.name);
+  return name.name;
+}
+
+
+async function getBanner() {
+  const banner = await chrome.storage.local.get(["banner"]);
+  console.log("THE banner IS", banner.banner);
+  return banner.banner;
+}
+
 
 async function changeProfilePictures() {
-  const gifUrl = await getInfo();
-  console.log("THE URL IS" + gifUrl);
+  const gifUrl = await getGifUrl();
+  const idNum = await getId();
+  const username = await getName();
+  const bannerUrl = await getBanner();
+  //console.log("THE URL IS" + gifUrl);
+  //console.log("THE ID IS" + idNum);
+
+  /*
+  For future decoration implemenation
+  const decoration = document.createElement("img");
+  decoration.className = "avatarDecoration_c19a55";
+  decoration.src = "https://cdn.discordapp.com/avatar-decoration-presets/a_44d96dca4f514777925f23d841f36fac.png?size=300&passthrough=false";
+  */
 
   const avatars = document.querySelectorAll('img[class*="avatar"], img[src*="cdn.discordapp.com/avatars"]');
   console.log("Found avatars:", avatars.length);
@@ -26,11 +55,34 @@ async function changeProfilePictures() {
   avatars.forEach(avatar => {
     if (avatar.src === gifUrl) return;
 
-    if (avatar.src.includes(`/avatars/${YOUR_USER_ID}/`)) {
+    if (avatar.src.includes(`/avatars/${idNum}/`)) { //SWITCH TO idNum once have sol
       console.log("Updating avatar to", gifUrl);
       avatar.src = gifUrl;
     }
   });
+  
+
+  /*
+  For possible banner implemntation - missing unique identifier
+  const bannerDiv = document.querySelector('.banner__68edb');
+  console.log("Updating banner to", gifUrl);
+  bannerDiv.style.backgroundImage = "url(https://i.imgur.com/2sSL9dy.jpeg)";
+  */
+
+  /*
+  Testing for banner implentation by using username
+  */
+  const profilePopout = document.querySelector(`div[aria-label="${username}"]`);
+  const bannerDiv = profilePopout.querySelector(".banner__68edb");
+  bannerDiv.style.backgroundImage = `url(${bannerUrl})`;
+
+  /*
+  Code for the popout theme
+  */
+  const popoutWhole = document.querySelector(".outer_c0bea0 ");
+  popoutWhole.className = "outer_c0bea0 theme-light images-light user-profile-popout custom-theme-background custom-user-profile-theme";
+  popoutWhole.style = "--profile-gradient-primary-color: hsla(36, 100%, 50%, 1); --profile-gradient-secondary-color: hsla(237, 100%, 30%, 1); --profile-gradient-overlay-color: #ffffff99; --profile-gradient-button-color: hsla(26, 100%, 28%, 1); --theme-base-color-light-hsl: 36.470588235294095 100% 90%; --theme-base-color-light: rgb(255,235,204); --theme-text-color-light: rgb(0,7,128); --theme-base-color-dark-hsl: 237.6923076923077 100% 5.098039215686274%; --theme-base-color-dark: rgb(0,1,26); --theme-text-color-dark: rgb(255,245,229); --theme-base-color-amount: 50%; --theme-text-color-amount: 25%; --bg-overlay-selected: unset; --bg-overlay-hover: unset; --bg-overlay-active: unset;";
+
 }
 
 
--- popup.js
+++ popup.js
@@ -1,9 +1,94 @@
 document.addEventListener("DOMContentLoaded", () => {
   const gifUpload = document.getElementById('gifUrl');
   const idUpload = document.getElementById('userId');
-  const saveButton = document.getElementById('saveButton');
+  //const updateButton = document.getElementById('updateButton');
+  const saveIdButton = document.getElementById('saveId');
+  const saveGifButton = document.getElementById('saveGif');
+
+  const nameUpload = document.getElementById('nameInput');
+  const bannerUpload = document.getElementById('bannerInput');
+  const saveNameButton = document.getElementById('saveName');
+  const saveBannerButton = document.getElementById('saveBanner');
+
+
+  saveIdButton.addEventListener('click', () => {
+    const id = idUpload.value.trim();
+
+    if (!id){
+      alert("Please enter an ID");
+    }
+    else {
+      chrome.storage.local.set({
+        'userId' : id
+      }).then(() => {
+        console.log("ID Set");
+      })
+
+      clearId();     
+      document.getElementById('idStatus').textContent = "Saved ID!";
+    }
+  })
+
+
+  saveGifButton.addEventListener('click', () => {
+    const gif = gifUpload.value.trim();
+
+    if (!gif){
+      alert("Please enter a GIF");
+    }
+    else {
+      chrome.storage.local.set({
+        'gif' : gif
+      }).then(() => {
+        console.log("GIF Set");
+      })
+
+      clearGif();
+      document.getElementById('gifStatus').textContent = "Saved Gif!";
+    }
+  })
+
+
+  saveNameButton.addEventListener('click', () => {
+    const name = nameUpload.value.trim();
+
+    if (!name){
+      alert("Please enter a name");
+    }
+    else {
+      chrome.storage.local.set({
+        'name' : name
+      }).then(() => {
+        console.log("Name Set");
+      })
+
+      clearId();     
+      document.getElementById('nameStatus').textContent = "Saved Name!";
+    }
+  })
+
+
+  saveBannerButton.addEventListener('click', () => {
+    const banner = bannerUpload.value.trim();
+
+    if (!banner){
+      alert("Please enter a GIF URL");
+    }
+    else {
+      chrome.storage.local.set({
+        'banner' : banner
+      }).then(() => {
+        console.log("Banner Set");
+      })
+
+      clearId();     
+      document.getElementById('bannerStatus').textContent = "Saved GIF!";
+    }
+  })
   
-  saveButton.addEventListener('click', () => {
+  /*
+  Old event listener for when events were not updated individually
+  updateButton.addEventListener('click', () => {
     const id = idUpload.value.trim();
     const gif = gifUpload.value.trim();
     console.log("The ID is: " + id);
@@ -18,10 +103,17 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Values set");
     });
   })
+
+  */
+
 })
 
 
-function clearFields() {
+
+function clearGif() {
   document.getElementById("gifUrl").value = "";
+}
+
+function clearId() {
   document.getElementById("userId").value = "";
 }
